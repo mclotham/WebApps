ff58b0e111923092bb6c136fe84f0d410ca35b10
Matthew Lothamer <mclotham@hotmail.com>
2021-04-16 10:48:32 -0400

    Added Razor Pages web app Added Razor Pages application.

---
+++ ContosoUniv/ContosoUniv.WebAppRp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ContosoUniv.WebAppRp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ContosoUniv.WebAppRp.Areas.Identity.IdentityHostingStartup))]
+namespace ContosoUniv.WebAppRp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
